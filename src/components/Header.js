ce2364d591af0ec421ea671e6d3ddc2cd22c7f24
Bhagyashri <bhagyashrideoghare116@gmail.com>
2023-03-29 23:00:48 +0530

    created a serchbar with search utube api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,74 @@
-import { useDispatch } from "react-redux"
-import { hamburger_url, userIconIrl, youtubeUrl } from "../utils/imgurl"
-import { toggleAction } from "../utils/menuSlice"
-
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { YOUTUBE_SEARCH_API } from "../utils/config";
+import { hamburger_url, userIconIrl, youtubeUrl } from "../utils/imgurl";
+import { toggleAction } from "../utils/menuSlice";
 
 const Header = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestion] = useState([]);
+  const [showSuggestion, setShowSuggestion] = useState(false);
 
-    const toggleMenuHandler = () => {
-        dispatch(toggleAction.toggleMenu())
-    }
-    return (
-        <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+  useEffect(() => {
+    //console.log(searchQuery)
+    const timer = setTimeout(() => getSearchSuggestion(), 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestion = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    //console.log(json)
+    setSuggestion(json[1]);
+  };
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleAction.toggleMenu());
+  };
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-2 ">
-        <img className="h-14 cursor-pointer" src={hamburger_url} alt="logo" onClick={() => toggleMenuHandler()}/>
-        <img className="h-14 mx-3" src={youtubeUrl} alt="/"/>
+        <img
+          className="h-14 cursor-pointer"
+          src={hamburger_url}
+          alt="logo"
+          onClick={() => toggleMenuHandler()}
+        />
+        <img className="h-14 mx-3" src={youtubeUrl} alt="/" />
       </div>
       <div className="col-span-10 px-10">
-        <input className="w-1/2 border border-slate-500 p-1 rounded-l-full" type="text" placeholder="search here"  />
-        <button className="border border-slate-500 rounded-r-full p-1">search</button>
+        <input
+          className="w-1/2 border border-slate-500 p-1 rounded-l-full"
+          type="text"
+          placeholder="search here"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setShowSuggestion(true)}
+          onBlur={() => setShowSuggestion(false)}
+        />
+        <button className="border border-slate-500 rounded-r-full p-1">
+          search
+        </button>
+        {showSuggestion && (
+          <div className="fixed bg-white pt-8 px-5 py-2 w-[37rem] shadow-lg rounded-lg ">
+            <ul>
+              {suggestions.map((s, index) => (
+                <li key={index}>{s}</li>
+              ))}
+            </ul>
+            
+          </div>
+        )}
       </div>
-      <div className="col-span-1">
-        <img className="h-10" src ={userIconIrl} alt="/" />
-      </div>
-        </div>
 
-    )
-}
-export default Header
+      <div className="col-span-1">
+        <img className="h-10" src={userIconIrl} alt="/" />
+      </div>
+    </div>
+  );
+};
+export default Header;
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,6 @@
 const api = "AIzaSyCa4rz2WF-jdTy2z2cI90bw7cxT6S-O654"
 
 export const youTubeApi = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key="+ api 
+
+export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
   
